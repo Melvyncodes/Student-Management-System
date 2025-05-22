838d6b2d0404df5211b8a40e15d6f450ad26c562
Melvin Atsiangbe <Timeyinsoft@gmail.com>
2025-05-22 23:29:02 +0100

    Revert "Update students-controller.js"

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,40 +2,28 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    const students = await getAllStudents();
-    res.status(200).json(students);
+    //write your code
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    const newStudent = await addNewStudent(req.body);
-    res.status(201).json({ message:'Student added', data: newStudent });
+    //write your code
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    const updated = await updateStudent(id, req.body);
-    res.status(200).josn({ message: 'Student Updated', data: updated });
+    //write your code
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const student = await getStudentDetail(id);
-    if (!student) {
-        res.status(404);
-        throw new Error('Student not found');
-  }
-  res.status(200).json(student);
+    //write your code
+
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const { status } = req.body;
-  const updated = await setStudentStatus(id, status);
-  res.status(200).json({ message: 'Status updated', data: updated });
-    
+    //write your code
+
 });
 
 module.exports = {
